a795896a55135117d5e0618282ed6d4979fe4ce5
Abdullaaaaah <abdullahkiet89@gmail.com>
2024-04-24 18:17:06 +0500

    adding Cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
 const trilaterationRoutes = require('./routes/trilaterationRoutes');
 const testRoutes = require('./routes/testRoutes');
 const distanceRoutes = require('./routes/distanceRoutes');
 
-
 const app = express();
 const port = process.env.PORT || 6611;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 app.use(dataRoutes);
