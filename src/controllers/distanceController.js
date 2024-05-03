33d21e5e0e5558d9ba5f4df5a7d1aaa8a0e78968
Abdullaaaaah <abdullahkiet89@gmail.com>
2024-05-03 12:56:17 +0500

    changes

--- src/controllers/distanceController.js
+++ src/controllers/distanceController.js
@@ -11,9 +11,24 @@ async function handleDistance(req, res) {
         // Fetch data from mongodbService based on the transmitterSerialNumbers
         const distances = [];
         for (const serialNumber of transmitterSerialNumbers) {
-            const latestData = await mongodbService.getDataByTransmitterSerialNumber(serialNumber);
-            const latestDistance = latestData ? latestData.distance : null;
-            distances.push({ transmitterSerialNumber: serialNumber, distance: latestDistance });
+            const jsonData = await mongodbService.getDataByTransmitterSerialNumber(serialNumber);
+            if (!jsonData) {
+                // If no data found for the transmitter serial number, push null distance
+                distances.push({ transmitterSerialNumber: serialNumber, distance: null });
+            } else {
+                // Find the latest read for the transmitter serial number
+                const latestRead = jsonData.reads.reduce((acc, curr) => {
+                    return (acc.timeStampUTC > curr.timeStampUTC) ? acc : curr;
+                }, {});
+
+                if (!latestRead) {
+                    // If no latest read found, push null distance
+                    distances.push({ transmitterSerialNumber: serialNumber, distance: null });
+                } else {
+                    // Push the latest distance for the transmitter serial number
+                    distances.push({ transmitterSerialNumber: serialNumber, distance: latestRead.distance });
+                }
+            }
         }
 
         res.json(distances);
