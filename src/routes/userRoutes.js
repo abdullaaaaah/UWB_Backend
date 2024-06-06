73ce0d23e220f6ea983266af0d23c34a1dc38986
Abdullaaaaah <abdullahkiet89@gmail.com>
2024-06-06 16:29:55 +0500

    changes mad

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const multer = require('multer');
+// Configure Multer storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+  });
+
+  const upload = multer({ storage });
 
 router.post('/signup', userController.registerController);
 router.post('/login', userController.loginController);
 router.post('/logout', userController.logoutController);
-router.post('/updateprofile', userController.updateProfileController);
+router.post('/updateprofile', upload.single('profilePic'), userController.updateProfileController);
 router.get('/getProfile', userController.getProfileController);
 module.exports = router;
